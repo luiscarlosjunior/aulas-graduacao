7e6af70b9c2faa642d29159715a715ecf9b02fc3
Luis Santos <luis.carlosjr@hotmail.com.br>
2020-04-10 07:48:19 -0300

    Adicionado os projetos feitos em aula

---
+++ Desenvolvimento Java/ExemploJavaSimples/src/exemplojavasimples/ExemploJavaSimples.java
@@ -0,0 +1,153 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemplojavasimples;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author luisc
+ */
+public class ExemploJavaSimples {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       // exemploEntrada();
+       // media();
+       exemplosVariaveis();
+       
+    }
+    
+    private static void exemploBreak() {
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+              break;
+            }
+            System.out.println(i);
+        }
+    }
+    
+    private static void exemploArrayMultiInt() {
+        int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+        for (int i = 0; i < myNumbers.length; ++i) {
+          for(int j = 0; j < myNumbers[i].length; ++j) {
+            System.out.println(myNumbers[i][j]);
+          }
+        }
+    }
+    
+    private static void exemploForContinue() {
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+              continue;
+            }
+            System.out.println(i);
+        }
+    }
+    
+    private static void exemploForLoop() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }     
+    }
+    
+    private static void exemploForEachLoop() {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (String i : cars) {
+          System.out.println(i);
+        }
+    }
+    
+    private static void exemploWhileLoop() {
+        int i = 0;
+        while (i < 5) {
+          System.out.println(i);
+          i++;
+        }  
+    }
+    
+    private static void exemploDoWhile() {
+        int i = 0;
+        do {
+          System.out.println(i);
+          i++;
+        }
+        while (i < 5);
+    }
+    
+    private static void exemploSwitch(){
+        int day = 4;
+        switch (day) {
+          case 1:
+            System.out.println("Monday");
+            break;
+          case 2:
+            System.out.println("Tuesday");
+            break;
+          case 3:
+            System.out.println("Wednesday");
+            break;
+          case 4:
+            System.out.println("Thursday");
+            break;
+          case 5:
+            System.out.println("Friday");
+            break;
+          case 6:
+            System.out.println("Saturday");
+            break;
+          case 7:
+            System.out.println("Sunday");
+            break;
+        }
+    }
+    
+    private static void exemplosVariaveis(){
+        int meuNum = 5;               // integer (whole number)
+        float meuFloatNum = 5.99f;    // floating point number
+        char minhaLetra = 'D';         // character
+        boolean meuBool = true;       // boolean
+        String meuTexto = "Hello";     // String    
+        System.out.println(meuNum);
+        System.out.println(meuFloatNum);
+        System.out.println(minhaLetra);
+        System.out.println(meuBool);
+        System.out.println(meuTexto);
+    }
+    
+    private static void exemploEntrada(){
+        Scanner ler = new Scanner(System.in); // 2. instanciando e criando um objeto Scanner
+        int a, b;
+
+        System.out.printf("Informe o primeiro valor: ");
+        a = ler.nextInt(); // 3. entrada de dados (lendo um valor inteiro)
+
+        System.out.printf("Informe o segundo valor.: ");
+        b = ler.nextInt(); // 3. entrada de dados (lendo um valor inteiro)
+
+        System.out.printf("\nResultados:\n");
+        System.out.printf("%d + %d = %3d\n", a, b, (a + b));
+        System.out.printf("%d - %d = %3d\n", a, b, (a - b));
+        System.out.printf("%d * %d = %3d\n", a, b, (a * b));
+        System.out.printf("%d / %d = %3d (divisão inteira)\n", a, b, (a / b));
+        System.out.printf("%d / %d = %6.2f (divisão exata)\n", a, b, ((double)a / b));
+    }
+    
+    private static void media() {
+        double nota1, nota2;
+        double media;
+        
+        nota1 = Double.parseDouble(JOptionPane.showInputDialog("Digite a primeira nota"));
+        nota2 = Double.parseDouble(JOptionPane.showInputDialog("Digite a segunda nota"));
+        
+        media = (nota1 + nota2) / 2;
+        
+        JOptionPane.showMessageDialog(null, media);
+    }
+}
---
+++ Desenvolvimento Java/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,228 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+// Importar o pacote javauitil
+import java.util.Scanner;
+/**
+ *
+ * @author luisc
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Instanciar e criar um objeto Scanner usando padrão de entrada System.in
+       ativ03d();
+        
+    }
+    
+    private void ativ01() {
+        Scanner ler = new Scanner(System.in);
+        
+        double C = 0.0;
+        double F = 0.0;
+        
+        System.out.println("Informe o valor em graus Celsius");
+        C = ler.nextDouble();
+        
+        F = (9*C+160)/5;
+        
+        System.out.println("Fahrenheit: " + F);
+    }
+    
+    private static void ativ02a() {
+        int A, B;
+        
+        A = 10;
+        B = 20;
+        
+        if (A > B) {
+            System.out.println("A diferença entre o A e o B é de " + (A-B));
+        }
+        else {
+            System.out.println("A diferença entre o B e o A é de " + (B-A));
+        }
+    }
+    
+    private static void ativ02b(){
+        Scanner sc = new Scanner(System.in);
+        
+        int number;
+        
+        System.out.println("Informe um valor");
+        number = sc.nextInt();
+        
+        if (number < 0) {
+            System.out.println("O módulo do número: |" + 
+                    number + "| é " + (number * -1));
+        }
+        else
+        {
+            System.out.println("O módulo do número: |" + 
+                number + "| é " + number);
+        }
+    }
+
+    private static void ativ02c() {
+        Scanner sc = new Scanner(System.in);
+        int number = 0;
+        
+        System.out.println("Entre com um valor entre 1 e 9");
+        try {
+            number = sc.nextInt();
+        } catch (Exception e) {
+            System.out.println("Só é permitido valores numericos");
+        }
+                
+        if (number >= 1 && number < 9) {
+            System.out.println("O valor está na faixa permitida");
+        }
+        else
+        {
+            System.out.println("O valor não está na faixa permitida");
+        }
+    }
+    
+    private static void ativ02d() {
+        
+        int A, B, C;
+        
+        A = -10;
+        B = 50; 
+        C = 0;
+        
+        if (A <= B && A <= C) {
+            if (B <= C) {
+                System.out.format("%d, %d, %d", A, B, C);
+            }
+            else
+            {
+                System.out.format("%d, %d, %d", A, C, B);
+            }
+        } else if (B <= A && B <= C) {
+            if (A <= C) {
+                System.out.format("%d, %d, %d", B, A, C);
+            }
+            else
+            {
+                System.out.format("%d, %d, %d", B, C, A);
+            }
+        }else if (C <= A && C <= B) {
+            if (A <= B) {
+                System.out.format("%d, %d, %d", C, A, B);
+            }
+            else
+            {
+                System.out.format("%d, %d, %d", C, B, A);
+            }
+        }
+    }
+    
+    private static void ativ02e() {
+        Scanner sc = new Scanner(System.in);
+        
+        int A, B, C, D;
+        
+        System.out.println("Entre com um valor");
+        A = sc.nextInt();
+        System.out.println("Entre com um valor");
+        B = sc.nextInt();
+        System.out.println("Entre com um valor");
+        C = sc.nextInt();
+        System.out.println("Entre com um valor");
+        D = sc.nextInt();
+        
+        System.out.println("Os vsalore validos são");
+        
+        if (!(A%2 != 0 && A%3 != 0)) {
+            System.out.println(A);
+        }
+        if (!(B%2 != 0 && B%3 != 0)) {
+            System.out.println(B);
+        }
+        if (!(C%2 != 0 && C%3 != 0)) {
+            System.out.println(C);
+        }
+        if (!(D%2 != 0 && D%3 != 0)) {
+            System.out.println(D);
+        }
+    }
+    
+    private static void ativ03a() {
+        Scanner sc = new Scanner(System.in);
+        
+        int number;
+        
+        number = sc.nextInt();
+        
+        System.out.println("A tabuada do numero " + number + " é:");
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println(number + " X " + (i+1) + " = " + (number * (i+1)));
+        }
+    }
+    
+    private static void ativ03b() {
+        int soma = 0;
+        
+        /*
+        for (int i = 1; i <= 500; i++) {
+            if (i%2 == 0) {
+                soma = soma + i;
+            }
+        }
+        */
+        
+        /*
+        for (int i = 0; i <= 500; i += 2) {
+                soma = soma + i;  
+        }
+        */
+        
+        System.out.println("A soma de 1 a 500 é de " + soma);
+    }
+    
+    private static void ativ03c() {
+        Scanner sc = new Scanner(System.in);
+        int x, number = 0, maior = 0, menor = 0;
+        
+        while(number >= 0)
+        {
+            System.out.println("Informe um numero");
+            number = sc.nextInt();
+            
+            if (number < 0) {
+                break;
+            }
+            
+            if (number > maior) {
+                maior = number;
+            } else if (number <= menor){
+                menor = number;
+            }
+        }
+        
+        System.out.println("O maior numero é: " + maior + "\nO menor numero é: " + menor);
+    }
+    
+    private static void ativ03d() {
+        int fat = 1;
+        int j;
+        
+        for (int i = 1; i < 10; i+= 2) {
+            j = i;
+            while(j > 1) {
+                fat *= j;
+                j--;
+            }
+            System.out.println("Fatorial de i é " + fat);
+            fat = 1;
+        }
+    }
+}
