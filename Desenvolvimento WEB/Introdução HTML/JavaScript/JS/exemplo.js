2058de08cb341217581b78effb3d037d96024a78
luiscarlosjunior <luis.carlosjr@hotmail.com.br>
2020-04-28 18:45:38 -0300

    Added new pages

--- Desenvolvimento WEB/Introdução HTML/JavaScript/JS/exemplo.js
+++ Desenvolvimento WEB/Introdução HTML/JavaScript/JS/exemplo.js
@@ -4,16 +4,5 @@ function validateEmailAddress(email) {
 }
 
 function validate() {
-  document.write ("alô mundo !!!");
-  $("#result").text("");
-  var emailaddress = $("#email").val();
-  if (validateEmailAddress(emailaddress)) {
-    $("#result").text(emailaddress + " é valido:)");
-    $("#result").css("color", "green");
-  } else {
-    $("#result").text(emailaddress + " não está correto, por favor entre novamente:(");
-    $("#result").css("color", "red");
-  }
-  return false;
+  alert("alô mundo !!!");
 }
-$("#validate").bind("click", validate);
