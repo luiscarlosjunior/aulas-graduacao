a4808861f60398b1883b5d9d376b7f52d172ad62
luiscarlosjunior <luis.carlosjr@hotmail.com.br>
2021-03-24 08:39:24 -0300

    Adicionado novos arquivo java com exemplos simples da linguagem

---
+++ POO UML/Codigos/Aula 1/java/Exemplos/02 - Variaveis/App.java
@@ -0,0 +1,24 @@
+public class App {
+    /**
+	 * @author Prof. Luis Caparroz Santos
+	 * Exemplo de uso de variáveis
+	 */
+
+	// Para saber mais
+	// https://www.caelum.com.br/apostila-java-orientacao-objetos/variaveis-primitivas-e-controle-de-fluxo
+    public static void main(String[] args) {
+    	
+    	int meuNum = 5;               // Números inteiros
+        float meuFloatNum = 5.99f;    // Número de ponto flutuante
+        char minhaLetra = 'D';         // Caracter único
+        boolean meuBool = true;       // Boleano: True ou False
+        String meuTexto = "Hello";     // Sequência de caracteres
+        
+        // Exibir conteúdo
+        System.out.println(meuNum);
+        System.out.println(meuFloatNum);
+        System.out.println(minhaLetra);
+        System.out.println(meuBool);
+        System.out.println(meuTexto);
+    }
+}
